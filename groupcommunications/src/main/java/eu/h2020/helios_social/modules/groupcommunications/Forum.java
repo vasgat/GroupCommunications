eb0a90983e06b36433f51df89c7f12293ca0363c
vasgat <vasgat@gmail.com>
2020-03-04 13:23:21 +0200

    minor updates

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/Forum.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/Forum.java
@@ -1,21 +1,37 @@
 package eu.h2020.helios_social.modules.groupcommunications;
 
+import androidx.annotation.Nullable;
+
 import eu.h2020.helios_social.modules.groupcommunications.utils.ContactId;
 import eu.h2020.helios_social.modules.groupcommunications.utils.GroupId;
 
+/**
+ * A Sharable Forum
+ */
 public class Forum extends Group {
 
     private String name;
     private final byte[] salt;
-    private ContactId owner;
+    private ContactId administrator;
 
-    public Forum(GroupId id, String name, ContactId owner, byte[] salt) {
+    /**
+     * Creates a sharable Forum with a given name and salt
+     * @param id GroupId of the Forum
+     * @param name name of the forum
+     * @param administrator Peer Identifier of the administrator of the forum (admin can be change)
+     * @param salt
+     */
+    public Forum(GroupId id, String name, @Nullable ContactId administrator, byte[] salt) {
         super(id);
         this.name = name;
-        this.owner = owner;
+        this.administrator = administrator;
         this.salt = salt;
     }
 
+    /**
+     *
+     * @return name of the Forum
+     */
     public String getName() {
         return name;
     }
@@ -24,14 +40,26 @@ public class Forum extends Group {
         return salt;
     }
 
+    /**
+     * Returns the Peer Identifier of the Peer with administrator rights
+     * @return
+     */
     public ContactId getForumOwner() {
-        return owner;
+        return administrator;
     }
 
-    public void setForumOwner(ContactId contactId) {
-        this.owner = contactId;
+    /**
+     * Updates administator of the Forum
+     * @param contactId
+     */
+    public void setForumOwner(@Nullable ContactId contactId) {
+        this.administrator = contactId;
     }
 
+    /**
+     * Updates Forum name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/SecretForum.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/SecretForum.java
@@ -4,19 +4,33 @@ package eu.h2020.helios_social.modules.groupcommunications;
 import eu.h2020.helios_social.modules.groupcommunications.utils.ContactId;
 import eu.h2020.helios_social.modules.groupcommunications.utils.GroupId;
 
+/**
+ * A Non-sharable Forum (Private/Secret Forum)
+ */
 public class SecretForum extends Group {
 
     private String name;
     private final byte[] salt;
-    private final ContactId creator;
+    private final ContactId owner;
 
-    public SecretForum(GroupId id, String name, ContactId creator, byte[] salt) {
+    /**
+     * Contracts a new instance of SecretForum
+     * @param id GroupId of the SecretForum
+     * @param name name of the forum
+     * @param owner owner/creator of the SecretForum (cannot change)
+     * @param salt
+     */
+    public SecretForum(GroupId id, String name, ContactId owner, byte[] salt) {
         super(id);
         this.name = name;
-        this.creator = creator;
+        this.owner = owner;
         this.salt = salt;
     }
 
+    /**
+     * Get tbe name of the Forum
+     * @return
+     */
     public String getName() {
         return name;
     }
@@ -25,10 +39,18 @@ public class SecretForum extends Group {
         return salt;
     }
 
-    public ContactId getCreator() {
-        return creator;
+    /**
+     * Get Peer Identifier of Forum Owner
+     * @return Contact Identifier
+     */
+    public ContactId getOwner() {
+        return owner;
     }
 
+    /**
+     * Change Forum's name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
