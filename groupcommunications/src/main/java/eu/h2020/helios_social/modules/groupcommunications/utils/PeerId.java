3083da9cb3f2b84a53b080db0e4ea4a3a9aecc24
vasgat <vasgat@gmail.com>
2020-03-04 17:05:29 +0200

    renamed from ContactId

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/utils/PeerId.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/utils/PeerId.java
@@ -1,12 +1,12 @@
 package eu.h2020.helios_social.modules.groupcommunications.utils;
 
-public class ContactId extends Identifier {
+public class PeerId extends Identifier {
     /**
      * Label for hashing peers to calculate their identifiers.
      */
     public static final String LABEL = "eu.h2020.helios_social.modules.groupcommunications.lib/PEER_ID";
 
-    public ContactId(byte[] id) {
+    public PeerId(byte[] id) {
         super(id);
     }
 }
