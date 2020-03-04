5a7d03b60d8aa4ec5f7bc225d6d93cbfd6ae72bc
vasgat <vasgat@gmail.com>
2020-03-04 17:02:11 +0200

    add MessageBody

---
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/MessageBody.java
@@ -0,0 +1,8 @@
+package eu.h2020.helios_social.modules.groupcommunications;
+
+public class MessageBody {
+
+    public MessageBody() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
