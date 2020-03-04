dcf2c69b911fad5211eeaaea18a8a31a969daafd
vasgat <vasgat@gmail.com>
2020-03-04 17:01:28 +0200

    add ForumMember

---
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/ForumMember.java
@@ -0,0 +1,10 @@
+package eu.h2020.helios_social.modules.groupcommunications;
+
+import eu.h2020.helios_social.modules.groupcommunications.peer.Peer;
+
+public class ForumMember {
+
+    public ForumMember(Peer peer){
+
+    }
+}
