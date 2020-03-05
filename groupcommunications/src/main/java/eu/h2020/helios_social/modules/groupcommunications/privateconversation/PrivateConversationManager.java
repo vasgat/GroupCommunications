682f17a19214227eb6d18c4c9ec96dd291e4300d
vasgat <vasgat@gmail.com>
2020-03-05 15:54:17 +0200

    minor updates

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/privateconversation/PrivateConversationManager.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/privateconversation/PrivateConversationManager.java
@@ -2,21 +2,36 @@ package eu.h2020.helios_social.modules.groupcommunications.privateconversation;
 
 import java.util.Collection;
 
+import eu.h2020.helios_social.modules.groupcommunications.Group;
 import eu.h2020.helios_social.modules.groupcommunications.Message;
+import eu.h2020.helios_social.modules.groupcommunications.peer.Peer;
 import eu.h2020.helios_social.modules.groupcommunications.utils.GroupId;
 import eu.h2020.helios_social.modules.groupcommunications.utils.MessageId;
 import eu.h2020.helios_social.modules.groupcommunications.utils.PeerId;
 
+/**
+ * Interface for Private Conversation Manager
+ */
 public interface PrivateConversationManager {
 
     /**
+     * Called when a peer is added to Contacts and it creates & returns a new Group
+     *
+     * @param peer
+     * @return
+     */
+    Group addingContact(Peer peer);
+
+    /**
      * Stores a Private Message
+     *
      * @param message
      */
     void storePrivateMessage(Message message);
 
     /**
      * Returns the Private Conversation Identifier with the given peer
+     *
      * @param peerId
      * @return
      */
@@ -24,6 +39,7 @@ public interface PrivateConversationManager {
 
     /**
      * Returns the Peer Identifier based on the Private Conversation Identifier
+     *
      * @param groupId
      * @return
      */
@@ -31,6 +47,7 @@ public interface PrivateConversationManager {
 
     /**
      * Returns all conversation {@Link Message}s with the given peer
+     *
      * @param peerId Peer Identifier
      * @return
      */
@@ -38,12 +55,14 @@ public interface PrivateConversationManager {
 
     /**
      * Deletes all messages related with the given peer
+     *
      * @param peerId
      */
     void deleteAllMessages(PeerId peerId);
 
     /**
      * Deletes the given set of messages related with the given per.
+     *
      * @param peerId
      */
     void deleteMessages(PeerId peerId, Collection<MessageId> messageIds);
