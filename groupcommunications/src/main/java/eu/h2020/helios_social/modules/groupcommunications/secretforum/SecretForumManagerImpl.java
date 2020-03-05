124249876aa8a6fdb9083f096305a71e54f25272
vasgat <vasgat@gmail.com>
2020-03-05 19:53:24 +0200

    add SecretForumManagerImpl class

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/secretforum/SecretForumManagerImpl.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/secretforum/SecretForumManagerImpl.java
@@ -1,4 +1,45 @@
 package eu.h2020.helios_social.modules.groupcommunications.secretforum;
 
+import java.util.Collection;
+
+import eu.h2020.helios_social.modules.groupcommunications.utils.GroupId;
+
+/**
+ * TODO: Add Implementation for SecretForumManager
+ */
 public class SecretForumManagerImpl implements SecretForumManager {
+
+    public SecretForumManagerImpl(SecretForumFactoryImpl secretForumFactory) {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
+
+    @Override
+    public SecretForum addSecretForum(String name) {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
+
+    @Override
+    public void joinsSecretForum(SecretForum secretForum) {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
+
+    @Override
+    public void deletesSecretForum(SecretForum secretForum) {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
+
+    @Override
+    public SecretForum getSecretForum(GroupId gid) {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
+
+    @Override
+    public Collection<SecretForum> getAllSecretForums() {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
+
+    @Override
+    public Collection<SecretForumMember> getSecretForumMembers(GroupId groupId) {
+        throw new UnsupportedOperationException("Not supported yet");
+    }
 }
